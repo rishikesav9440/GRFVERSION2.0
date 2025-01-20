da7511f85673ef61a256a0518e87b82bb7d5ac5b
Rishi Kesav <121454665+rishikesav9440@users.noreply.github.com>
2025-01-20 17:38:39 +0530

    Update Header2.jsx

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="p-4" style={{ backgroundColor: '#222131' }}> {/* Dark purple background */}
+    <div className="fixed top-0 left-0 right-0 p-4 z-50" style={{ backgroundColor: '#222131' }}> {/* Dark purple background */}
       <div className="flex justify-center items-center">
         <img 
           src="logo.jpeg" 
